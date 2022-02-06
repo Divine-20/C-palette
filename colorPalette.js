13fd95bf4780632143c891b3be18e4273a1fbd49
Ingabire Divine <divineingabire69@gmail.com>
2022-02-06 18:39:19 +0200

    added a link

--- colorPalette.js
+++ colorPalette.js
@@ -1,5 +1,5 @@
 function generatePalette(){
-    location.href ="https://divine-20.github.io/ColorPalette/"
+    location.href ="file:///C:/Users/pc/OneDrive/Documents/myCodes/Revis/design/challenges/C-palette/index(2).html"
 
 }
 
